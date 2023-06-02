2688e623b453d342da30f0efe571b223eb02c4e6
alexisPingu <99997532+alexisPingu@users.noreply.github.com>
2023-06-02 14:35:13 -0600

    hashrouter

--- src/components/Agregartarea.jsx
+++ src/components/Agregartarea.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from 'react'
+import Swal from 'sweetalert2'
 
 export const Agregartarea = ({guardar,tareaSeleccionada,actualizar}) => {
     //console.log(tareaSeleccionada)
     const [descripcion,setDescripcion]=useState('')
 
     const guardarDatos=()=>{
-      if(tareaSeleccionada) actualizar({...tareaSeleccionada,descripcion:descripcion})
-      else guardar(descripcion)
-      setDescripcion(null)
+      if (descripcion.length>0 ) {
+        if(tareaSeleccionada) actualizar({...tareaSeleccionada,descripcion:descripcion})
+        else guardar(descripcion)
+        setDescripcion(null)
+      }else{
+        Swal.fire('Error','Tarea no valida','warning')
+      }
     }
     useEffect(()=>{
       if(tareaSeleccionada){
@@ -19,7 +24,7 @@ export const Agregartarea = ({guardar,tareaSeleccionada,actualizar}) => {
     <div className='col-7 ms-3 d-flex mb-2'>
         <div className="input-group input-group-lg">
             <label className='input-group-text'>Tarea</label>
-            <input type="text" className='form-control' onChange={(e)=>{setDescripcion(e.target.value)}} value={descripcion}  />
+            <input type="text" className='form-control' onChange={(e)=>{setDescripcion(e.target.value)}} value={descripcion}/>
             <button className='btn btn-outline-success fs-3' onClick={()=>{guardarDatos(descripcion)}}><i className='bi bi-plus-circle'></i></button>
         </div>
     </div>
